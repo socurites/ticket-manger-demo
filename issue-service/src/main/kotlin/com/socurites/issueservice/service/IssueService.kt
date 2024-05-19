423a01cd046d9df9557921da48a06e5dec9e3bc9
Aaron Song <aaron@kmong.com>
2024-05-19 10:12:41 +0900

    feat: Issue 수정

--- issue-service/src/main/kotlin/com/socurites/issueservice/service/IssueService.kt
+++ issue-service/src/main/kotlin/com/socurites/issueservice/service/IssueService.kt
@@ -37,4 +37,20 @@ class IssueService(
         val issue = issueRepository.findByIdOrNull(id) ?: throw NotFoundException("이슈가 존재하지 않습니다")
         return IssueResponse(issue)
     }
+
+    @Transactional
+    fun update(userId: Long, id: Long, request: IssueRequest): IssueResponse {
+        val issue = issueRepository.findByIdOrNull(id) ?: throw NotFoundException("이슈가 존재하지 않습니다")
+
+        return with(issue) {
+            summary = request.summary
+            description = request.description
+            this.userId = userId
+            type = request.type
+            priority = request.priority
+            status = request.status
+
+            IssueResponse(issueRepository.save(this))
+        }
+    }
 }
--- issue-service/src/main/kotlin/com/socurites/issueservice/web/IssueController.kt
+++ issue-service/src/main/kotlin/com/socurites/issueservice/web/IssueController.kt
@@ -8,6 +8,7 @@ import com.socurites.issueservice.service.model.IssueResponse
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -36,4 +37,11 @@ class IssueController(
         authUser: AuthUser,
         @PathVariable id: Long,
     ) = issueService.get(id)
+
+    @PutMapping("/{id}")
+    fun edit(
+        authUser: AuthUser,
+        @PathVariable id: Long,
+        @RequestBody request: IssueRequest,
+    ) = issueService.update(authUser.userId, id, request)
 }
