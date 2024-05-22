9ac987426ba127e7d2840b03fdf5bfc7481af813
Aaron Song <aaron@kmong.com>
2024-05-22 19:47:08 +0900

    feat: Hello handler 구현

---
+++ spring-webflux-playground/src/main/kotlin/com/socurites/webfluxplay/handler/Router.kt
@@ -0,0 +1,16 @@
+package com.socurites.webfluxplay.handler
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.RouterFunctions.route
+import org.springframework.web.reactive.function.server.ServerResponse
+
+@Configuration
+class Router {
+    @Bean
+    fun helloRouter(handler: HelloHandler): RouterFunction<ServerResponse> =
+        route()
+            .GET("/", handler::sayHello)
+            .build()
+}
