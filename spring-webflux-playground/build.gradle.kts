78277010ceca855a83937030748cd39e252e662f
Aaron Song <aaron@kmong.com>
2024-05-22 19:36:55 +0900

    config: Add spring webflux 의존성

--- spring-webflux-playground/build.gradle.kts
+++ spring-webflux-playground/build.gradle.kts
@@ -1,19 +1,7 @@
-plugins {
-    id("java")
-}
-
-group = "com.socurites"
-version = "0.0.1-SNAPSHOT"
-
-repositories {
-    mavenCentral()
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
 }
