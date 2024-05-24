7db03f70da2bf190c01033d0552e210beb1ea9c9
Aaron Song <aaron@kmong.com>
2024-05-24 11:22:44 +0900

    config: R2DBC config 설정

--- user-service/src/main/kotlin/com/socurites/userservice/UserServiceApplication.kt
+++ user-service/src/main/kotlin/com/socurites/userservice/UserServiceApplication.kt
@@ -2,9 +2,11 @@ package com.socurites.userservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(exclude = [R2dbcAutoConfiguration::class])
+@SpringBootApplication
+@ConfigurationPropertiesScan
 class UserServiceApplication
 
 fun main(args: Array<String>) {
